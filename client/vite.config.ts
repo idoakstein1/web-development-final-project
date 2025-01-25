b1b4cf953827ca20527adacb06924bfe17471899
idoakstein1 <idoakstein1@gmail.com>
2025-01-25 15:23:33 +0200

    updated files

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    // server: {
-    //     https:
-    //         process.env.NODE_ENV === 'production'
-    //             ? {
-    //                   key: `${process.env.PATH_TO_CERTS}client-key.pem`,
-    //                   cert: `${process.env.PATH_TO_CERTS}client-cert.pem`,
-    //               }
-    //             : undefined,
-    // },
+    server: {
+        port: 5137,
+        // https:
+        //     process.env.NODE_ENV === 'production'
+        //         ? {
+        //               key: `${process.env.PATH_TO_CERTS}client-key.pem`,
+        //               cert: `${process.env.PATH_TO_CERTS}client-cert.pem`,
+        //           }
+        //         : undefined,
+    },
 });
