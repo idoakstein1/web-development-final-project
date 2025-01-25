62425b9fe041acf9d3834003450f9ffe6ee979d9
idoakstein1 <idoakstein1@gmail.com>
2025-01-25 16:33:29 +0200

    changed port

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     preview: {
-        port: 4137,
+        port: 3000,
         host: '0.0.0.0',
         // https:
         //     process.env.NODE_ENV === 'production'
